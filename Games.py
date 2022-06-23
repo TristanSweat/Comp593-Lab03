5d32b910ef71f8bd3938f1f0fd09e9d5d9b0c410
TristanSweat <105946649+TristanSweat@users.noreply.github.com>
2022-06-23 08:04:39 -0400

    Update Games.py
    Attempted to add new dictionary titles "Ratings" with ratings of "ten" in lower().

--- Games.py
+++ Games.py
@@ -37,17 +37,46 @@ def main():
             'Comedy',
         ]        
     }
+
+    #Step 3 Adding Movie to Data Structure
+    new_title = ('The Hateful Eight')
+    add_title_to_movies(favorite_movies, new_title)
+    new_genre = ('Western')
+    add_genre_to_movies(favorite_movies, new_genre)
+    pass
+
+def add_title_to_movies(favorite_movies, new_title):
+    favorite_movies['Title'].append(new_title)
+def add_genre_to_movies(favorite_movies, new_genre):
+    favorite_movies['Genre'].append(new_genre)
+
     print(favorite_movies['Title'])
     print(favorite_movies['Genre'])
-    #Step 3 Adding Movie to Data Structure
-
-    #def add_movies_to_movies(Title, Genre): - Not Finished
 
     #Step 4 Add Movie characteristics to Data structure
+def add_ratings_to_movies(favorite_movies, ratings):
+    favorite_movies['Ratings'].append(ratings)
+
+    add_imdb_ratings = {'Ratings': "Ten"}
+    add_list = []
+
+    add_imdb_ratings = add_imdb_ratings.copy()
+    add_list.append(add_imdb_ratings)
+
+    pass
+    print(add_list)
+
+    for i,p in enumerate(favorite_movies['Ratings']):
+        favorite_movies['Ratings'][i] = p.lower()
+
+        return
+    print(favorite_movies['Ratings'])
+
+
 
     #Step 5 Function to print bullet points of games - Not finished
-def print_video_games(video_games):
-    print('These are my 3 favorite games', video_games)
+#def print_video_games(video_games):
+    #print('These are my 3 favorite games', video_games)
 
 # Main function call
 main()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,23 @@
+    new_title = ('The Hateful Eight')
+    add_title_to_movies(favorite_movies, new_title)
+    new_genre = ('Western')
+    add_genre_to_movies(favorite_movies, new_genre)
+    pass
+
+def add_title_to_movies(favorite_movies, new_title):
+    favorite_movies['Title'].append(new_title)
+def add_genre_to_movies(favorite_movies, new_genre):
+    favorite_movies['Genre'].append(new_genre)
+
+    print(favorite_movies['Title'])
+    print(favorite_movies['Genre'])
+
+    #Step 4 Add Movie characteristics to Data structure
+    add_imdb_ratings = {'ratings'}
+    add_list = []
+
+    dictionary_copy = add_imbd_ratings.copy()
+    add_list = []
+
+    pass
+    print(add_list)
