7fc42d90163060415eddb2cc97ab11021df97dd4
TristanSweat <105946649+TristanSweat@users.noreply.github.com>
2022-06-23 23:17:09 -0400

    Update Games.py
    Updated Games.py to section 7 and 8.
    Cannot figure out how to get 7 and 8 to work properly and unfortunately have to leave for the weekend.
    Got up to section 6 working.

--- Games.py
+++ Games.py
@@ -9,24 +9,18 @@ def main():
     full_name = {
         'Name': 'Tristan Nichol',
     }
-
-    print(full_name['Name'])
     
-
     Student_id = {
-        'id': '32141251514',
+        'id': '10273523',
     }
 
-    print(Student_id['id'])
-
     video_games = {
-        'games': [
+        'Games': [
             'Final Fantasy VII',
             'Apex Legends',
             'Valorant',
         ]
     }
-    print(video_games['games'])
 
     favorite_movies = {
         'Title': [
@@ -40,73 +34,83 @@ def main():
         ]        
     }
 
-    #Step 3 Adding Movie to Data Structure
+    #Step 3 Adding Movie Title to Data Structure
     new_title = ('The Hateful Eight')
     add_title_to_movies(favorite_movies, new_title)
+
+    #Adding Moving Genre to Data Structure
     new_genre = ('Western')
     add_genre_to_movies(favorite_movies, new_genre)
-    
 
+    #Adding New Games to Games Data Structure
+    new_games = ('Tarkov', 'WoW', 'PubG')
+    add_games_to_games(video_games, new_games)
+
+    # Print information from the complex data structure
+    print_full_name(full_name)
+    print_student_id(Student_id)
+    print_video_games(video_games)
+    #print_favorite_movies(favorite_movies)
+
+    #Add new title to list
 def add_title_to_movies(favorite_movies, new_title):
     favorite_movies['Title'].append(new_title)
+
+    #Add new genre to list
 def add_genre_to_movies(favorite_movies, new_genre):
     favorite_movies['Genre'].append(new_genre)
 
-    print(favorite_movies['Title'])
-    print(favorite_movies['Genre'])
+    #Step 4 Add Games characteristics to Data structure
+def add_games_to_games(video_games, new_games):
+    video_games['Games'].extend(new_games)
 
-    #Step 4 Add Movie characteristics to Data structure
-def add_ratings_to_movies(favorite_movies, Ratings):
-    favorite_movies['Ratings'].append(Ratings)
+    #lower case first letter in each ratings movie
+    for i,p in enumerate(video_games['Games']):
+        video_games['Games'][i] = p.lower()
+    video_games['Games'].sort()
 
-    add_imdb_ratings = {'Ratings': ('Ten', 'Nine', 'Eight')}
-    add_list = []
-
-    add_imdb_ratings = add_imdb_ratings.copy()
-    add_list.append(add_imdb_ratings)
-
- 
-    print(add_list)
-
-    for i,p in enumerate(favorite_movies['Ratings']):
-        favorite_movies['Ratings'][i] = p.lower()
-
-    favorite_movies['Ratings'].sort()
-
-   
-    print(favorite_movies['Ratings'])
-
+new_games = {
+    'Tarkov',
+    'World of Warcraft',
+    'PubG',
+}
 
     #Step 5 Function to print full name
 def print_full_name(full_name):
-    sentence = f"My name is'{full_name['Name']}, but you can call me Sir{full_name['Name'].split(' ')[1]}"
-    print(sentence, end='n\n')
+    sentence = f"My name is {full_name['Name']}, but you can call me Sir {full_name['Name'].split(' ')[1]}"
+    print(sentence, end='\n')
+
+    #Step 5 Function to print ID
+def print_student_id(Student_id):
+    sentence = f"My Student Id is {Student_id['id']}"
+    print(sentence, end='\n')
 
     #Step 6 Function to print bullet points of games
 def print_video_games(video_games):
     #print heading
-    heading = f"{video_games['games']}"
+    heading = f"{video_games['Games']}"
     print(heading)
     print('-' * len(heading))
 
     #print bullet points of all games
-    for g in video_games['games']:
+    for g in video_games['Games']:
         print(f"- {g}")
     print()
     
 
-    pass
-    #7USE A FUNCTION TO PRINT A COMMA-SEPARATED LIST OF MOVIE GENRES
-def print_genre_list(favorite_movies ['Genre'])
+    
+    #step 7 use a function to print a comme-seperated list of movie genres
+#def print_favorite_movies(favorite_movies):
 
-    print (f"These are the Genres {favorite_movies ['Genre'].split(' ')[1][2][3]}", end='')
-    print (', '.join(f['Genre'] for f in favorite_movies['Genre']), end'.\n)
+    #print(f"These are the Genres {favorite_movies ['Genre'].split(' ')[1][2][3]}", end='')
+    #print(', '.join(g['Genre'] for g in favorite_movies['Genre']), end='.\n')
 
-    #8USE A FUNCTION TO PRINT A COMMA-SEPARATED LIST OF MOVIE TITLES
-def print_title_list(favorite_movies ['Title'])
+    
+    #step 8 use a function to print a comme-seperated list of movie titles
+#def print_title_list(favorite_movies ['Title']):
 
-    print (f"These are the Movies {favorite_movies ['Title'].split(' ')[1][2][3]}", end='')
-    print (', '.join(t['Title'] for t in favorite_movies['Title']), end'.\n)
+    #print(f"These are the Movies {favorite_movies ['Title'].split(' ')[1][2][3]}", end='')
+    #print(', '.join(t['Title'] for t in favorite_movies['Title']), end'.\n)
 
 # Main function call
 main()
